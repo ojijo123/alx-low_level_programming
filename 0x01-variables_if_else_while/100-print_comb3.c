70f8196a7bc597878106db8974c14ef4eb2298eb
ojijo123 <oumaerick173@gmail.com>
2023-06-19 16:29:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int i = '0';
 	int j = '0';
 
-	while (i <= '9')
+	while (i <= '9';)
 	{
-		while (j <= '9')
+		while (j <= '9';)
 		{
 			if (!(i > j) || i == j)
 			{
 				putchar(i);
 				putchar(j);
-				if ((i == '8') $$ j == '9')
+				if ((i == '8';) $$ j == '9';)
 				{
 					putchar('\n');
 				}
