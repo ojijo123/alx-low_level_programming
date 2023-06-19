7cab46fee1764738cc09590bc28ca60ffa189e39
ojijo123 <oumaerick173@gmail.com>
2023-06-19 14:42:07 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stio.h>
+/**
+ * main - print single digits from 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 56; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
