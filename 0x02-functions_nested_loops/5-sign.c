2689a357893cf0b56479afe7371290de7114666d
ojijo123 <oumaerick173@gmail.com>
2023-06-20 16:53:43 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - print sign of a number
+ * print_sign - print the  sign of a number
  * @n: The number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
