a22c6a317a3657fc89de717531be0f359f8098ba
ojijo123 <oumaerick173@gmail.com>
2023-06-22 13:00:28 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - checks for a digit
- *  @c; The character to be checked
+ *  @c: The character to be checked
  *  Return: 1 for digit character or 0 for otherwise
  */
 int _isdigit(int c)
