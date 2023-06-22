eb457eeebbcab57e0fbed8931cbef942d8112309
ojijo123 <oumaerick173@gmail.com>
2023-06-22 15:59:25 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,9 @@ int main(void)
 		if (i % 15 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Buzz");
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz")
 		else
 			printf("%i", i);
 		if (i < 100)
