707b8e18d7b72a9a7775fe8e2156eb25a9f913ce
ojijo123 <oumaerick173@gmail.com>
2023-06-23 12:02:06 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - test for a postitive_or_negative
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+	return (0);
+}
