657caa369985b156f3875b9ec0eac191013ce2e6
ojijo123 <oumaerick173@gmail.com>
2023-06-26 14:36:42 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+include "main.h"
+/**
+ * puts2 - prints every other character of a string,
+ * @str: inputs
+ * Return: print string
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *i = s;
+	int o;
+
+	while (*i != '\0')
+	{
+		i++;
+		longi++;
+	}
+	t - longi = 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
