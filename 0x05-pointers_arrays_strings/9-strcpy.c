dc843b4f4f2aab6e8a1ffe20430a32a668d91f5e
ojijo123 <oumaerick173@gmail.com>
2023-06-26 17:09:10 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + 1) != '\0')
 	{
-		i++;
+		1++;
 	}
 	for ( ; x < 1 ; x++)
 	{
-		dest[x] = src[x]
+		dest[x] = src[x];
 	}
 	dest[x] = '\0';
 	return (dest);
