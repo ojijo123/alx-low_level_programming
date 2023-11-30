949fca44e4f01c54783f550ad5576edd5b361cd2
ojijo123 <oumaerick173@gmail.com>
2023-11-30 00:06:13 -0800

    read_textfile.c

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -9,22 +9,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-        int fd;
-        ssize_t nrd, nwr;
-        char *buff;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
 
-        if (!filename)
-                return (0);
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
-        buff = malloc(sizeof(char) * letters);
-        if (buff == NULL)
-                return (0);
-        nrd = read(fd, buff, letters);
-        nwr = write(STDOUT_FILENO, nrd, buff);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, nrd, buff);
 
-        close(fd);
-        free(buff);
-        return (nwr);
+	close(fd);
+	free(buff);
+	return (nwr);
 }
