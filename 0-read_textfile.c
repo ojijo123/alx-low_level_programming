cff995e99214240392be6624bb0458cea29e8d1b
ojijo123 <oumaerick173@gmail.com>
2023-11-30 00:38:34 -0800

    read_textfile.c

--- 0-read_textfile.c
+++ 0-read_textfile.c
@@ -9,7 +9,7 @@
  * prints it to the POSIX standard output
  *
  * @filename: filename (pointer to an array)
- * 0-read_textfile.c@letters: letters to be printed
+ * @letters: letters to be printed
  * Return: number of letters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -18,18 +18,23 @@ ssize_t read_textfile(const char *filename, size_t letters);
 	ssize_t nrd, nwr;
 	char *buff;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
+	{
+		close(fd);
 		return (0);
+	}
 	nrd = read(fd, buff, letters);
 	nwr = write(STDOUT_FILENO, nrd, buff);
 
 	close(fd);
+	if (nrd == -1)
 	free(buff);
+	return (0);
 	return (nwr);
 }
