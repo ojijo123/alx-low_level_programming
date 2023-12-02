0a151197e06c01e374119c0878662130ec9b45f8
ojijo123 <oumaerick173@gmail.com>
2023-12-02 08:15:45 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,37 @@
-#include "main.h"
-
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 /**
- * create_file-program created
- * @filename: filename created
- * @text_content: content of the file
+ * create_file - creates a file and puts text in it
  *
- *Return: 1 if success or -1 if fail
+ * @filename: filename
+ * @text_content: text to put into file
+ *
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_d;
-	int nletters;
-	int rwr;
+	int file;
+	int length = 0, inlen = 0;
+	char *ptr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	file_d = 0pen(filename, o_CREAT | o_WRONLY | TRUNC, 0600);
-	if (file_d == -1)
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
-	if (!text_content)
-		text_content = "";
-	for (nletters = 0; text_content(nletters); nletters++)
-		;
-	rwr = write(file_d, text_content, nletters);
-	if (rwr == -1)
+
+	if (text_content != NULL)
+	{
+		for (inlen = 0, ptr = text_content; *ptr; ptr++)
+			inlen++;
+		length = write(file, text_content, inlen);
+	}
+
+	if (close(file) == -1 || inlen != length)
 		return (-1);
-	close(file_d);
 	return (1);
 }
